e4afc3e3b58d2d7c3573d64c06f51ea61ee467e7
Aidan Houlihan <aidandhoulihan@gmail.com>
2020-03-29 20:33:00 +1300

    Added support for Quoted chars

--- csv/csv.py
+++ csv/csv.py
@@ -5,20 +5,62 @@ but written fully in python
 """
 import io
 
-def reader(csvfile, dialect='excel', **fmtparams):
+def reader(
+    csvfile, deliminter=',', newline='\n', quotechar='"'
+):
     """
     Reader class accepts an iterable object for csvfile
     """
-
     try:
         csv_iterable = iter(csvfile)
-    except TypeError:
-        raise Exception('csvfile must be of an iterable type')
+    except TypeError as e:
+        raise TypeError('csvfile must be of an iterable type')
 
     csv_row_list = []
 
     if hasattr(csvfile, 'read'):
-        csv_row_list = csvfile.read().split('\n')
+        csv_rows = csvfile
+    else:
+        # CSV is already in an iterable format
+        csv_rows = csvfile
 
-    for row in csvfile:
-        yield row
+    for string_row in csvfile:
+        yield _convert_string_to_columns(
+            string_row, newline, deliminter, quotechar,
+        )
+
+
+def _convert_string_to_columns(string_row, deliminter, newline, quotechar):
+    """
+    Taking a generated string row return back the formatted
+    resulting CSV columns.
+    """
+    columns = []
+
+    num_chars = 0
+
+    while num_chars < len(string_row):
+        current_column_value = ''
+
+        for index, char in enumerate(string_row[num_chars:]):
+            if char == quotechar:
+                sub_string = string_row[num_chars:]
+
+                # Preform look ahead for next quote char,
+                # search to next quotechar if present.
+                next_quote_pos = sub_string[index:].find(quotechar)
+                if next_quote_pos != -1:
+                    quoted_chars = sub_string[next_quote_pos:]
+                    current_column_value += quoted_chars.rstrip()
+                    num_chars += 1
+                    break
+            elif char == deliminter or char == newline:
+                num_chars += 1
+                break
+
+            current_column_value += char
+
+        columns.append(current_column_value)
+        num_chars += len(current_column_value)
+
+    return columns
--- csv/test_csv.py
+++ csv/test_csv.py
@@ -6,16 +6,52 @@ from csv import reader
 class TestReader(unittest.TestCase):
 
     def test_reader_loads_file_object(self):
-        csv_strings = [
+        file_io = io.StringIO('my,test,strings\nmy,second,rows')
+
+        csv_reader = reader(file_io)
+
+        excpected_csv_strings = [
             ['my', 'test', 'strings'],
             ['my', 'second', 'rows'],
         ]
-        f = io.StringIO('my,test,strings\nmy,second,rows')
-
-        csv_reader = reader(f)
 
         for index, row_list in enumerate(csv_reader):
-            self.assertEqual(csv_strings[index], row_list)
+            self.assertEqual(
+                excpected_csv_strings[index], row_list,
+            )
+
+    def test_reader_loads_non_file_iterables(self):
+        csv_lists = ['my,test,strings', 'my,second,rows']
+
+        csv_reader = reader(csv_lists)
+
+        excpected_csv_strings = [
+            ['my', 'test', 'strings'],
+            ['my', 'second', 'rows'],
+        ]
+
+        for index, row_list in enumerate(csv_reader):
+            self.assertEqual(
+                excpected_csv_strings[index], row_list,
+            )
+
+    def test_reader_ignores_quote_characters(self):
+        file_io = io.StringIO('my,"test,strings"\nmy,second,rows')
+        csv_reader = reader(file_io)
+
+        excpected_csv_strings = [
+            ['my', '"test,strings"'],
+            ['my', 'second', 'rows'],
+        ]
+
+        for index, row_list in enumerate(csv_reader):
+            self.assertEqual(
+                excpected_csv_strings[index], row_list,
+            )
+
+    def test_non_iterable_raises_exception(self):
+        csv_reader = reader(None)
+        self.assertRaises(TypeError, lambda: next(csv_reader))
 
 
 if __name__ == '__main__':
